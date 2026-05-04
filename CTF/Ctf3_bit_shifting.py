daf9388ed2d1bde2202950f1dee777341caf051b
farahsharshar <Farah.hassan05@eng-st.cu.edu.eg>
2026-05-04 03:56:47 +0300

    *CTF3

---
+++ CTF/Ctf3_bit_shifting.py
@@ -0,0 +1,25 @@
+def solve(filepath):
+    with open(filepath, 'r') as f:
+        numbers=list(map(int, f.read().split()))
+    c=0
+    ascii=[]
+    for i in numbers:
+        if i>126:
+            ascii.append(i)
+        if i % 2 == 0:
+            c+=1
+    even = c == len(numbers)
+    decoded= []
+    for n in numbers:
+        original = n >> 1
+        char= chr(original)
+        decoded.append(char)
+    flag = ''.join(decoded)
+    print(f"\nFLAG: {flag}")
+    return flag
+
+
+if __name__ == "__main__":
+    import sys
+    filepath = sys.argv[1] if len(sys.argv) > 1 else "shifted.txt"
+    solve(filepath)
